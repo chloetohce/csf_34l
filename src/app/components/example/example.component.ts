8de3cdbdd562e56b0d8af997fe7919889ef1fc4e
chloetohce <chloetohce@gmail.com>
2025-02-27 10:00:18 +0800

    init

---
+++ src/app/components/example/example.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-example',
+  standalone: false,
+  templateUrl: './example.component.html',
+  styleUrl: './example.component.css'
+})
+export class ExampleComponent {
+
+}
